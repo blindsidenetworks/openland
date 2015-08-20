4c4792805f074460d8ca3031691a7077384c5418
jfederico <jesus@123it.ca>
2015-08-20 10:33:30 -0400

    Added username to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820143114) do
+ActiveRecord::Schema.define(version: 20150820143254) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -34,9 +34,11 @@ ActiveRecord::Schema.define(version: 20150820143114) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "username",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
