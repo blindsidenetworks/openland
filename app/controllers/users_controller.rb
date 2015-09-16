42bd0e507d51725a14c31be19058f023c621bdc3
jfederico <jesus@123it.ca>
2015-09-16 17:34:35 -0400

    Implemented actions in user_controller and their corresponding views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+
   end
 
   def update
@@ -26,5 +34,10 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  private
+    def user_params
+        params.require(:user).permit(:username, :email, :first_name, :last_name, :password)
+    end
 
 end
