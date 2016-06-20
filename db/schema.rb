c6074511ff55362f5f3e1b0291359ff595714a55
jfederico <jesus@123it.ca>
2016-06-19 22:07:14 -0700

    Extended database for supporting oauth authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,47 +11,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007151051) do
+ActiveRecord::Schema.define(version: 20160619191648) do
 
   create_table "rooms", force: :cascade do |t|
-    t.string   "name",               limit: 255
-    t.text     "description",        limit: 65535
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.integer  "user_id",            limit: 4
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "user_id"
     t.boolean  "recording"
-    t.string   "welcome",            limit: 255
-    t.string   "moderator_password", limit: 255
-    t.string   "viewer_password",    limit: 255
+    t.string   "welcome"
+    t.string   "moderator_password"
+    t.string   "viewer_password"
     t.boolean  "wait_moderator"
-    t.string   "permissions",        limit: 255
+    t.string   "permissions"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.string   "username",               limit: 255
-    t.integer  "roles_mask",             limit: 4
-    t.string   "first_name",             limit: 255
-    t.string   "last_name",              limit: 255
-    t.string   "picture_file_name",      limit: 255
-    t.string   "picture_content_type",   limit: 255
-    t.integer  "picture_file_size",      limit: 4
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "username"
+    t.integer  "roles_mask"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
