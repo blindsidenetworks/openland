a1ffbc5a090bf72ed549d15ff93cdb56203db24c
jfederico <jesus@123it.ca>
2016-06-20 07:38:41 -0700

    fixed auto generation of username on oauth autocration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,36 @@ class User < ActiveRecord::Base
   has_attached_file :picture, :styles => { :medium => "150x150", :small => "48x48", :icon => "32x32"},
                     :url  => "/assets/users/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/users/:id/:style/:basename.:extension"
+
+  include ActionView::Helpers
+
   ## For paperclip
   #do_not_validate_attachment_file_type :picture
   #validates_attachment_presence :picture
   validates_attachment_size :picture, :less_than => 5.megabytes
   validates_attachment_content_type :picture, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :omniauthable, # :validatable,
+         :omniauth_providers => [:twitter, :facebook]
+
+  #devise :database_authenticatable, :registerable, :confirmable, :rememberable,
+  #              :recoverable, :trackable, :omniauthable, # :validatable,
+  #              :authentication_keys => [:subdomain, :login],
+  #              :request_keys => [:subdomain],
+  #              :reset_password_keys => [:subdomain, :login],
+  #              :omniauth_providers => [:digitalocean]
+  def self.from_omniauth(auth)
+      where(:provider => auth.provider, :uid => auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email || ""
+        user.password = Devise.friendly_token[0,20]
+        user.username = unique_username(auth.info.email)
+      end
+  end
 
   ## Roles implemented using role_model
   # 0000 0001 =  1 = [:admin]
@@ -47,6 +66,10 @@ class User < ActiveRecord::Base
       :case_sensitive => false
     }
 
+  #def email_required?
+  #  super && provider.blank?
+  #end
+
   def login=(login)
     @login = login
   end
@@ -73,5 +96,18 @@ class User < ActiveRecord::Base
 
   def to_s
     (self.fullname != '')? self.fullname: (self.username != '')? self.username: self.email
+  end
+
+  private
+  def self.unique_username(email)
+    if email != nil
+      username = email.split("@").first
+    else
+      username = random_password(8)
+    end
+    while User.find_by(:username => username) do
+      username = random_password(8)
+    end
+    username
   end
 end
